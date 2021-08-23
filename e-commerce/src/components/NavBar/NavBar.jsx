6dd79199a2d392c1dbf17443f794760787e53c29
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-23 01:07:29 -0600

    Add routes

--- e-commerce/src/components/NavBar/NavBar.jsx
+++ e-commerce/src/components/NavBar/NavBar.jsx
@@ -1,27 +1,31 @@
 import {AppBar, Toolbar, IconButton,Badge, MenuItem, Menu, Typography} from "@material-ui/core";
 import {ShoppingCart} from "@material-ui/icons";
+import {Link, useLocation} from "react-router-dom";
 
 import useStyles from "./styles";
 import logo from "../../assets/commerce.png";
 
 const NavBar = ({totalItems}) => {
 	const classes = useStyles();
+	const location = useLocation();
 	return (
 		<>
 			<AppBar position={"fixed"} className={classes.appBar} color={"inherit"}>
 				<Toolbar>
-					<Typography variant={"h6"} className={classes.title} color={"inherit"}>
+					<Typography component={Link} to={"/"} variant={"h6"} className={classes.title} color={"inherit"}>
 						<img src={logo} alt={"ECommerce"} height={"25px"} className={classes.image}/>
 						ECommerce
 					</Typography>
 					<div className={classes.grow}/>
-					<div className={classes.button}>
-						<IconButton aria-label={"Show cart items"} color={"inherit"}>
-							<Badge badgeContent={totalItems} color={"secondary"}>
-								<ShoppingCart/>
-							</Badge>
-						</IconButton>
-					</div>
+					{location.pathname === "/" && (
+						<div className={classes.button}>
+							<IconButton component={Link} to={"/cart"} aria-label={"Show cart items"} color={"inherit"}>
+								<Badge badgeContent={totalItems} color={"secondary"}>
+									<ShoppingCart/>
+								</Badge>
+							</IconButton>
+						</div>
+					)}
 				</Toolbar>
 			</AppBar>
 		</>
