edc03277ce340a4f955084ad932b24c103786c47
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-23 00:11:58 -0600

    Add to cart functionality

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -4,18 +4,27 @@ import {Products, NavBar} from "./components";
 
 const App = () => {
 	const [products, setProducts] = useState([]);
+	const [cart, setCart] = useState({});
 	const fetchProducts = async () => {
 		const {data} = await commerce.products.list();
 		setProducts(data);
 	};
+	const fetchCart = async () => {
+		setCart(await commerce.cart.retrieve());
+	};
+	const handleAddToCart = async (productId, quantity) => {
+		const item = await commerce.cart.add(productId, quantity);
+		setCart(item.cart);
+	}
 	useEffect(() => {
 		fetchProducts();
+		fetchCart();
 	}, []);
 
 	return (
 		<div>
-			<NavBar/>
-			<Products products={products}/>
+			<NavBar totalItems={cart.total_items}/>
+			<Products products={products} onAddToCart={handleAddToCart}/>
 		</div>
 	)
 };
