7d33c063b1483158f4e4a8c48191f360b7a5d2a0
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-23 21:11:54 -0600

    Add buttons in checkout page

--- e-commerce/src/components/CheckoutForm/AddressForm.jsx
+++ e-commerce/src/components/CheckoutForm/AddressForm.jsx
@@ -1,10 +1,11 @@
 import {useEffect, useState} from "react";
 import {InputLabel, Select, MenuItem, Button, Grid, Typography} from "@material-ui/core";
 import {useForm, FormProvider, useFormContext} from "react-hook-form";
+import {Link} from "react-router-dom";
 import FormInput from "./FormInput";
 import {commerce} from "../../lib/commerce";
 
-const AddressForm = ({checkoutToken}) => {
+const AddressForm = ({checkoutToken, next}) => {
 	const [shippingCountries, setShippingCountries]= useState([]);
 	const [shippingCountry, setShippingCountry]= useState("");
 	const [shippingSubdivisions, setShippingSubdivisions]= useState([]);
@@ -51,7 +52,7 @@ const AddressForm = ({checkoutToken}) => {
 		<>
 			<Typography variant={"h6"} gutterBottom>Shipping Address</Typography>
 			<FormProvider {...methods}>
-				<form onSubmit="">
+				<form onSubmit={methods.handleSubmit((data) => next({...data, shippingCountry, shippingSubdivision, shippingOption}))}>
 					<Grid container spacing={3}>
 						<FormInput name={"firstName"} label={"First Name"}/>
 						<FormInput name={"lastName"} label={"Last Name"}/>
@@ -84,6 +85,11 @@ const AddressForm = ({checkoutToken}) => {
 							</Select>
 						</Grid>
 					</Grid>
+					<br/>
+					<div style={{display: "flex", justifyContent: 'space-between'}}>
+						<Button component={Link} to={"/cart"} variant={"outlined"}>Back to Cart</Button>
+						<Button type={"submit"} variant={"contained"} color={"primary"}>Next</Button>
+					</div>
 				</form>
 			</FormProvider>
 		</>
--- e-commerce/src/components/CheckoutForm/Checkout/Checkout.jsx
+++ e-commerce/src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -12,12 +12,12 @@ const Checkout = ({cart}) => {
 	const classes = useStyles();
 	const [activeStep, setActiveStep] = useState(0);
 	const [checkoutToken, setCheckoutToken] = useState(null);
+	const [shippingData, setShippingData] = useState({});
 
 	useEffect(() => {
 		const generateToken = async () => {
 			try {
 				const token = await commerce.checkout.generateToken(cart.id, {type: "cart"});
-				console.log(token);
 				setCheckoutToken(token);
 			} catch (error) {
 
@@ -26,10 +26,20 @@ const Checkout = ({cart}) => {
 		generateToken();
 	}, [cart]);
 
+	const nextStep = () => setActiveStep((preActiveStep) => preActiveStep + 1);
+	const backStep = () => setActiveStep((preActiveStep) => preActiveStep - 1);
+
+	const next = (data) => {
+		setShippingData(data);
+		nextStep();
+	};
+
 	const Confirmation = () => (
 		<div>Confirmation</div>
 	);
-	const Form = () => activeStep === 0 ? <AddressForm checkoutToken={checkoutToken}/> : <PaymentForm/>;
+	const Form = () => activeStep === 0
+		? <AddressForm checkoutToken={checkoutToken} next={next}/>
+		: <PaymentForm shippingData={shippingData}/>;
 	return (
 		<>
 			<div className={classes.toolbar}/>
