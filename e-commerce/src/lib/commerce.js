b298ab91da0e963c0b43e3bdab2661929b3106c1
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-23 22:00:59 -0600

    Connected stripe, and env bug fixes

--- e-commerce/src/lib/commerce.js
+++ e-commerce/src/lib/commerce.js
@@ -1,3 +1,3 @@
 import Commerce from "@chec/commerce.js";
 
-export const commerce = new Commerce("pk_32266f3ab4128b3db913a4199ac63269b8b3726147fc4", true);
+export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
