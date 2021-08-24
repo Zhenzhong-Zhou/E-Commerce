c4108f22b6169ed541ce17dac549762fd3b8d972
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-23 21:00:56 -0600

    Add fecth options in checkout page

--- e-commerce/src/components/CheckoutForm/FormInput.jsx
+++ e-commerce/src/components/CheckoutForm/FormInput.jsx
@@ -1,12 +1,12 @@
 import {TextField, Grid, Select, MenuItem, InputLabel} from "@material-ui/core";
 import {useFormContext, Controller} from "react-hook-form";
 
-const FormInput = ({name, label, required}) => {
+const FormInput = ({name, label}) => {
 	const {control} = useFormContext();
 	return (
 		<Grid item xs={12} sm={6}>
 			<Controller render={({field}) => (
-				<TextField {...field} label={label} required={required}/>
+				<TextField {...field} label={label} required/>
 			)} control={control} fullWidth name={name}/>
 		</Grid>
 	)
